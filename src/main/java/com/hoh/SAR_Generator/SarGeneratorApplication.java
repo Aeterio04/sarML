f37a58e97dd7dd5e631a677a1c90bfb7052bafdb
Soham <soham0809@gmail.com>
2026-03-13 16:35:18 +0530

    Initial commit

---
+++ src/main/java/com/hoh/SAR_Generator/SarGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.hoh.SAR_Generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoh/SAR_Generator/SarGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoh.SAR_Generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
